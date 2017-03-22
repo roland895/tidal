21201ccca2d7efd1a9932f55f79b8d3c01045bf2
roland895 <rl1369@nyu.edu>
2017-03-22 14:27:57 -0400

    tidal assignment

--- app/app.js
+++ app/app.js
@@ -1,14 +1,76 @@
-'use strict';
+(function(angular) {
+  'use strict';
+  var tidalApp = angular.module('tidalApp',[]);
+  //CONTROLLERS
+  tidalApp.controller('mainController',["$scope", function($scope) {
+    this.slide = {
+      name: "Future",
+      txt:"Kanye West is our one and only god.",
+      img:"img/future.png",
+      verb:"LISTEN NOW"
+    };
+    this.slide2 = {
+      name:"Experience DS2 Live",
+      txt:"All hail Kanye West: Our lord and saviour.",
+      img:"img/Brush.png",
+      verb:"BUY TICKETS"
+    }
+    this.slide3 = {
+      name:"Watch exclusive videos.",
+      txt:"Praise to Kayne",
+      img:"img/Screenshot.png",
+      verb:"WATCH VIDEOS"
+    }
+    $scope.features = [
+      this.slide,
+      this.slide2,
+      this.slide3
+    ]
+    $scope.nav = [
+      {item: "Something", href: "#"},
+      {item: "Web Player", href: "#"}
+    ]
+  }]);
+  function albumFeatureController() {
+  }
+  tidalApp.component('albumFeature', {
+      templateUrl: "templates/albumFeature.html",
+      controller: albumFeatureController,
+      bindings:{
+        name: '=',
+        img:'=',
+        txt:'=',
+        verb:'='
+      }
+    }
+    
+    );
+  function popupSectionController(){
+    var id = this.id;
+    console.dir(this);
+    var waypoint = new Waypoint({
+      element: document.getElementById(id),
+      handler: function(direction) {
+        this.elemtent.className="toggle-bounce";
+      },
+      offset: 100
+    });
+  };
+  tidalApp.component('popUpSection', {
+      templateUrl: "templates/popUpSectionPhones.html",
+       controller: popupSectionController,
+      bindings:{
+        id: "@"
+      }
+  });
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    
+  $('.dropdown').on('show.bs.dropdown', function(e){
+    $(this).find('button').toggleClass("open");
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+  });
+  $('.dropdown').on('hide.bs.dropdown', function(e){
+    $(this).find('button').toggleClass("open");
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+  });
+})(window.angular);
